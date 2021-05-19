b8e96d596c4783e1c131329213aea3e52abf6af5
lennylv <lvyongwenhouzi@qq.com>
2021-05-19 22:54:35 +0800

    [11]盛最多水的容器

---
+++ src/main/java/com/lvyongwenhouzi/leetcode/leetcode/editor/cn/ContainerWithMostWater.java
@@ -0,0 +1,83 @@
+//给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i,
+//ai) 和 (i, 0) 。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+//
+// 说明：你不能倾斜容器。
+//
+//
+//
+// 示例 1：
+//
+//
+//
+//
+//输入：[1,8,6,2,5,4,8,3,7]
+//输出：49
+//解释：图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为 49。
+//
+// 示例 2：
+//
+//
+//输入：height = [1,1]
+//输出：1
+//
+//
+// 示例 3：
+//
+//
+//输入：height = [4,3,2,1,4]
+//输出：16
+//
+//
+// 示例 4：
+//
+//
+//输入：height = [1,2,1]
+//输出：2
+//
+//
+//
+//
+// 提示：
+//
+//
+// n = height.length
+// 2 <= n <= 3 * 104
+// 0 <= height[i] <= 3 * 104
+//
+// Related Topics 数组 双指针
+// 👍 2462 👎 0
+
+
+package com.lvyongwenhouzi.leetcode.leetcode.editor.cn;
+
+public class ContainerWithMostWater {
+    public static void main(String[] args) {
+        Solution solution = new ContainerWithMostWater().new Solution();
+        System.out.println(solution.maxArea(new int[]{1, 2, 3, 4, 5}));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int maxArea(int[] height) {
+
+            int x1 = 0;
+            int x2 = height.length -1 ;
+            int maxArea = (x2 - x1) * Math.min(height[x1], height[x2]);
+
+            while (x1 < x2) {
+
+                if (height[x1] > height[x2]) {
+                    x2 = x2 - 1;
+                } else {
+                    x1 = x1 + 1;
+                }
+                maxArea = Math.max(maxArea,(x2 - x1) * Math.min(height[x1], height[x2]));
+            }
+
+
+            return maxArea;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
