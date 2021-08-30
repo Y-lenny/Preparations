ada9dbb3be6d3b1d6e65ccab735f5132408fe6c0
吕永文 <11114396@bogon.vivo.xyz>
2021-08-30 14:12:28 +0800

    调整目录

---
+++ src/main/java/com/lvyongwenhouzi/PreparationsApplication.java
@@ -0,0 +1,13 @@
+package com.lvyongwenhouzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreparationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreparationsApplication.class, args);
+	}
+
+}
