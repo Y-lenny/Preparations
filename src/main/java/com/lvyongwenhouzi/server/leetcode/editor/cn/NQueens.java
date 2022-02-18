5c7a68df8c646f979fe9fc5813e1274f222dbe31
lvyongwen <lvyongwenhouzi@qq.com>
2022-02-18 20:26:30 +0800

    提交回溯算法： 定义 回溯的处理思想，有点类似枚举搜索。我们枚举所有的解，找到满足期望的解。为了有规律地枚举所有可能的解，避免遗漏和重复，我们把问题求解的过程分为多个阶段。每个阶段，我们都会面对一个岔路口，我们先随意选一条路走，当发现这条路走不通的时候（不符合期望的解），就回退到上一个岔路口，另选一种走法继续走。   算法场景 深度优先搜索、正则表达式匹配、编译原理  应用场景 数独、八皇后、0-1 背包、图的着色、旅行商问题、全排列

--- src/main/java/com/lvyongwenhouzi/server/leetcode/editor/cn/NQueens.java
+++ src/main/java/com/lvyongwenhouzi/server/leetcode/editor/cn/NQueens.java
@@ -39,18 +39,98 @@
 
 package com.lvyongwenhouzi.server.leetcode.editor.cn;
 
-import java.util.List;
+import java.util.*;
 
-public class NQueens{
-  public static void main(String[] args) {
-       Solution solution = new NQueens().new Solution();
-  }
-  //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public List<List<String>> solveNQueens(int n) {
-        return null;
+public class NQueens {
+    public static void main(String[] args) {
+        Solution solution = new NQueens().new Solution();
+        // System.out.println(solution.solveNQueens(5));;
     }
-}
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public List<List<String>> solveNQueens(int n) {
+            // 保存皇后可以排布的集合列表
+            List<List<String>> queens = new ArrayList<>();
+            // 创建冲突集合，提高判断是否存在攻击可能
+            int[] rows = new int[n];
+            Arrays.fill(rows, -1);
+            Set<Integer> columns = new HashSet<>();
+            Set<Integer> slash1 = new HashSet<>();
+            Set<Integer> slash2 = new HashSet<>();
+            backtracking(0, n, queens, columns, slash1, slash2, rows);
+            return queens;
+        }
+
+        /**
+         * 回溯方法
+         * @param row   行
+         * @param n 皇后矩阵
+         * @param queens    皇后组合集合
+         * @param columns   列互斥
+         * @param slash1    左下斜线互斥
+         * @param slash2    右下斜线互斥
+         * @param rows      皇后位置
+         */
+        private void backtracking(int row, int n, List<List<String>> queens, Set<Integer> columns, Set<Integer> slash1, Set<Integer> slash2, int[] rows) {
+            if (row == n) {
+                // 表示已经回溯到了可以满足互不冲突的组合
+                List<String> board = transChar(row, rows);
+                queens.add(board);
+            } else {
+                for (int i = 0; i < n; i++) {
+
+                    // 列互相冲突
+                    if (columns.contains(i)) {
+                        continue;
+                    }
+                    // 左下斜线互相冲突
+                    int s1 = row - i;
+                    if (slash1.contains(s1)) {
+                        continue;
+                    }
+                    // 右下斜线互相冲突
+                    int s2 = row + i;
+                    if (slash2.contains(s2)) {
+                        continue;
+                    }
+
+                    // 保存行皇后标识，并把斜线、列值保存
+                    rows[row] = i;
+                    columns.add(i);
+                    slash1.add(s1);
+                    slash2.add(s2);
+
+                    // 继续下一行皇后排列
+                    backtracking(row + 1, n, queens, columns, slash1, slash2, rows);
+
+                    // 回溯上一步
+                    rows[row] = -1;
+                    columns.remove(i);
+                    slash1.remove(s1);
+                    slash2.remove(s2);
+                }
+            }
+        }
+
+        /**
+         * 字符转字符串
+         * @param n
+         * @param rows 皇后位置
+         * @return
+         */
+        private List<String> transChar(int n, int[] rows) {
+            List<String> queen = new ArrayList<>();
+            char[] c = new char[n];
+            for (int i = 0; i < n; i++) {
+                Arrays.fill(c, '.');
+                c[rows[i]] = 'Q';
+                queen.add(String.valueOf(c));
+            }
+            return queen;
+        }
+    }
+
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
